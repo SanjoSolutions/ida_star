71c9d3cb79d8d108ec17e6983c8cbe622c4d23af
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-06-22 17:03:00 +0200

    IDA*

---
+++ README.md
@@ -0,0 +1,7 @@
+# IDA*
+
+This work is devoted to God.
+
+Please look at how the arguments are used in order to understand, what's required.
+
+The implementation uses tree traversal, to avoid max stack size reached errors that have been encountered with a recursive implementation.
---
+++ index.js
@@ -0,0 +1,94 @@
+export function search(
+  root,
+  estimateTravelDistance,
+  isSolution,
+  requestChildren,
+  requestParent
+) {
+  let solution
+  let maximumTravelDistance = estimateTravelDistance(root)
+  let iterationNumber = 1
+  do {
+    const result = iteration(
+      root,
+      estimateTravelDistance,
+      isSolution,
+      requestChildren,
+      requestParent,
+      maximumTravelDistance
+    )
+    solution = result.solution
+    if (maximumTravelDistance === result.nextMaximumTravelDistance) {
+      return null
+    } else {
+      maximumTravelDistance = result.nextMaximumTravelDistance
+    }
+    iterationNumber++
+  } while (!solution)
+
+  return solution
+}
+
+function iteration(
+  root,
+  estimateTravelDistance,
+  isSolution,
+  requestChildren,
+  requestParent,
+  maximumTravelDistance
+) {
+  let node = root
+  let nextMaximumTravelDistance = Infinity
+  while (true) {
+    if (isSolution(node)) {
+      return {
+        solution: node,
+        nextMaximumTravelDistance: null
+      }
+    } else {
+      const estimatedTotalTravelDistance = estimateTravelDistance(node)
+      if (estimatedTotalTravelDistance > maximumTravelDistance) {
+        nextMaximumTravelDistance = Math.min(
+          nextMaximumTravelDistance,
+          estimatedTotalTravelDistance
+        )
+      }
+      if (
+        estimatedTotalTravelDistance <= maximumTravelDistance &&
+        requestChildren(node).length >= 1
+      ) {
+        node = requestChildren(node)[0]
+      } else {
+        let parent = requestParent(node)
+        while (parent) {
+          const parentChildren = requestChildren(parent)
+          const childIndex = parentChildren.indexOf(node)
+          if (childIndex === -1) {
+            throw new Error(
+              'Could not find child node in children. ' +
+              'Please make sure that references to the same nodes stay the same ' +
+              'between multiple requestChildren calls for the same node.'
+            )
+          }
+          const maximumChildIndex = parentChildren.length - 1
+          if (childIndex < maximumChildIndex) {
+            const nextChildIndex = childIndex + 1
+            const nextChild = parentChildren[nextChildIndex]
+            node = nextChild
+            break
+          } else {
+            node = parent
+            node.children = []
+            parent = requestParent(node)
+          }
+        }
+        if (!parent) {
+          return {
+            solution: null,
+            nextMaximumTravelDistance
+          }
+        }
+      }
+    }
+  }
+}
